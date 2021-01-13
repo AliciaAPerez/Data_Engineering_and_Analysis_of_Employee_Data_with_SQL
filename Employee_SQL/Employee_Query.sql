a286d39dd8055319aecd623d9a9b67c6f31df5cb
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-01-12 22:51:41 -0600

    updating files with details

--- Employee_SQL/Employee_Query.sql
+++ Employee_SQL/Employee_Query.sql
@@ -1,6 +1,6 @@
---Data analysis employee query 
+-- Data analysis employee query 
 
--- Quick Reference for databases and columns names:
+-- Quick Reference for tables and columns names:
 -- titles(t): title_id, title
 -- departments(d): dept_number, dept_name
 -- employees(e): employee_number, title_id, birth_date, first_name, last_name, sex, hire_date
@@ -8,18 +8,18 @@
 -- dept_employee(de): employee_number, dept_number
 -- salaries(s): employee_number, salary
 
--- 1 employee details: employee number, last name, first name, sex, salary
+-- Query 1 - employee details: employee number, last name, first name, sex, salary
 SELECT e.employee_number, e.last_name, e.first_name, e.sex, s.salary
 FROM employees AS e
 LEFT JOIN salaries AS s
 ON e.employee_number = s.employee_number;
 
--- 2 employee details: first name, last name, hire date for employees hired in 1986
+-- Query 2 - employee details: first name, last name, hire date for employees hired in 1986
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE EXTRACT(YEAR FROM hire_date) = '1986';
 
--- 3 manager details: department number, department name, manager's employee number, last name, first name
+-- Query 3 - manager details: department number, department name, manager's employee number, last name, first name
 SELECT d.dept_number, d.dept_name, dm.employee_number, e.last_name, e.first_name
 FROM departments AS d
 LEFT JOIN dept_manager AS dm
@@ -27,7 +27,7 @@ ON d.dept_number = dm.dept_number
 LEFT JOIN employees AS e
 ON e.employee_number = dm.employee_number;
 
--- 4 employee department details: employee number, last name, first name, department name
+-- QUery 4 - employee department details: employee number, last name, first name, department name
 SELECT e.employee_number, e.last_name, e.first_name, d.dept_name
 FROM departments AS d
 LEFT JOIN dept_employee AS de
@@ -35,13 +35,13 @@ ON d.dept_number = de.dept_number
 LEFT JOIN employees AS e
 on e.employee_number = de.employee_number;
 
--- 5 first name, last name, sex employees with first name "Hercules" and last name being with "B"
+-- Query 5 - first name, last name, sex employees with first name "Hercules" and last name being with "B"
 SELECT first_name, last_name, sex
 FROM employees 
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
 
--- 6 employees in sales department with employee number, last name, first name, department name
+-- Query 6 - employees in sales department with employee number, last name, first name, department name
 -- employees does not include managers
 SELECT e.employee_number, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
@@ -51,7 +51,7 @@ LEFT JOIN departments AS d
 ON d.dept_number = de.dept_number
 WHERE d.dept_name = 'Sales';
 
--- 7 all employees in sales and development departments with employee number, last name, first name, department name
+-- Query 7 - all employees in sales and development departments with employee number, last name, first name, department name
 -- employees does not include managers
 SELECT e.employee_number, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
@@ -62,7 +62,7 @@ ON d.dept_number = de.dept_number
 WHERE d.dept_name = 'Sales' 
 OR d.dept_name = 'Development';
 
--- 8 descending order, frequency count of last names
+-- Query 8 - in descending order, frequency count of last names
 SELECT last_name, COUNT(last_name) AS "Frequency of Last Names"
 FROM employees
 GROUP BY last_name
--- Employee_SQL/Employee_Schema.sql
+++ Employee_SQL/Employee_Schema.sql
@@ -1,7 +1,10 @@
 --employee database table setup
 --setting up six tables for six csv files
 
---starting with titles table setup
+--Order for tables based on data and primary & foreign keys: 
+--(1)titles, (2)departments, (3)employees, (4)dept_managers, (5)dept_employee, (6)salaries
+
+--Setup Table 1 - titles
 DROP TABLE IF EXISTS titles;
 CREATE TABLE titles(	
 	title_id VARCHAR(5) NOT NULL,
@@ -10,7 +13,7 @@ CREATE TABLE titles(
 );
 SELECT * FROM titles;
 
---departments table setup
+--Setup Table 2 - departments
 DROP TABLE IF EXISTS departments;
 CREATE TABLE departments(
 	dept_number VARCHAR(4) NOT NULL,
@@ -19,47 +22,50 @@ CREATE TABLE departments(
 );
 SELECT * FROM departments;
 
---employees table setup
+--Setup Table 3 - employees
 DROP TABLE IF EXISTS employees;
 CREATE TABLE employees(
 	employee_number INTEGER NOT NULL,
 	title_id VARCHAR (5) NOT NULL,
-	birth_date DATE,
+	birth_date DATE NOT NULL,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR (30) NOT NULL,
-	sex VARCHAR(1),
-	hire_date DATE,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
 	PRIMARY KEY (employee_number),
 	FOREIGN KEY (title_id) REFERENCES titles(title_id)	
 );
 SELECT * FROM employees;
 
---department manager table setup
+--Setup Table 4 - department_manager
 DROP TABLE IF EXISTS dept_manager;
 CREATE TABLE dept_manager(
 	dept_number VARCHAR (5) NOT NULL,
 	employee_number INTEGER NOT NULL,
 	FOREIGN KEY (dept_number) REFERENCES departments(dept_number),
-	FOREIGN KEY (employee_number) REFERENCES employees(employee_number)
+	FOREIGN KEY (employee_number) REFERENCES employees(employee_number),
+	PRIMARY KEY (dept_number, employee_number)
 );
 SELECT * FROM dept_manager;
 
---department employee table setup
+--Setup Table 5 - department_employee
 DROP TABLE IF EXISTS dept_employee;
 CREATE TABLE dept_employee(
 	employee_number INTEGER NOT NULL,
 	dept_number VARCHAR(5) NOT NULL,
 	FOREIGN KEY (employee_number) REFERENCES employees(employee_number),
-	FOREIGN KEY (dept_number) REFERENCES departments(dept_number)
+	FOREIGN KEY (dept_number) REFERENCES departments(dept_number),
+	PRIMARY KEY (employee_number, dept_number)
 );
 SELECT * FROM dept_employee;
 
---salaraties table setup
+--Setup Table 6 - salaraties
 DROP TABLE IF EXISTS salaries;
 CREATE TABLE salaries(
 	employee_number INTEGER NOT NULL,
 	salary INTEGER NOT NULL,
-	FOREIGN KEY (employee_number) REFERENCES employees(employee_number)
+	FOREIGN KEY (employee_number) REFERENCES employees(employee_number),
+	PRIMARY KEY (employee_number, salary)
 );
 SELECT * FROM salaries;
 
